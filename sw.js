890af30444409662103b22de352a5400293b1773
wernerjakobsen-max <wernerjakobsen@gmail.com>
2025-11-07 13:42:06 +0100

    Add files via upload
    Grader lett synlig

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ts-kalkulator-v3-3';
+const CACHE = 'ts-kalkulator-v3-4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-lister-blikk.png','./logo-lister-blikk@2x.png'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
