26c1ca993bf773a43daffc2a29e4a5b15180ebf8
wernerjakobsen-max <wernerjakobsen@gmail.com>
2025-11-08 09:21:16 +0100

    Add files via upload
    Skjuler kalibrering

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ts-kalkulator-v3-4-3';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-lister-blikk.png','./logo-lister-blikk@2x.png','./qr.html'];
+const CACHE = 'ts-kalkulator-v3-6';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-lister-blikk.png','./qr.html'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))); });
