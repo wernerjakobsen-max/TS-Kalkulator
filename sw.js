e988aa4ec1ec6442b588256b8639bfe67b54050d
wernerjakobsen-max <wernerjakobsen@gmail.com>
2025-11-09 12:30:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ts-kalkulator-v3-6-3';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-lister-blikk.png','./qr.html'];
+const CACHE = 'ts-kalkulator-v3-6-4';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg','./logo-lister-blikk.svg','./qr.html'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))); });
